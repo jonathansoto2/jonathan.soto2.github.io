dfefc434caf4d7b24b3d9600580d2510892a8e34
Jonny Mac <soto.jonathan6@gmail.com>
2019-05-20 13:03:15 -0400

    api get request
    the javascript file should access the json file from instagrams api

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+var DATE = Date();
+
+var ACCESS_TOKEN = "31465581.2075d8b.17907f14606b43ad93aa5c0cfc320513";
+
+var request = new XMLHttpRequest();
+
+request.open("GET", "https://api.instagram.com/v1/users/self/?access_token=31465581.2075d8b.17907f14606b43ad93aa5c0cfc320513", true);
+
+request.onload = function(){
+    var data = JSON.parse(this.response);
+
+    console.log(data.bio);
+
+};
+
+
+request.send();
+
 console.log("hello work pls");
+console.log(DATE);
